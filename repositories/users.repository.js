1749d720680d36df44f48d38d0916941e52462fa
JangKroed <didlsdydgh@gmail.com>
2022-10-20 12:58:22 +0900

    주석 수정, DTO 추가 ?

--- repositories/users.repository.js
+++ repositories/users.repository.js
@@ -1,13 +1,15 @@
 const { User } = require('../models');
 
 class UserRepository {
+  constructor() {
+    this.User = User;
+  }
+
   /**
    * 유저 정보를 DB에 저장
-   * @param {*} nickname 
-   * @param {*} password 
    */
-  createUser = async (nickname, password) => {
-    await User.create({
+  createUser = async ({nickname, password}) => {
+    await this.User.create({
       nickname,
       password,
     });
@@ -15,11 +17,9 @@ class UserRepository {
 
   /**
    * 유저 정보중 닉네임을 기준으로 찾아준다
-   * @param {*} nickname 
-   * @returns 
    */
-  findByUser = async (nickname) => {
-    const user = await User.findOne({
+  findByUser = async ({nickname}) => {
+    const user = await this.User.findOne({
       where: {
         nickname,
       },
